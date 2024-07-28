b9a155476504adda797a2101cf37e3a8369db9d4
Collin Brennan <collinbrennan8@gmail.com>
2024-07-27 20:41:00 -0400

    Add basic REPL

---
+++ repl/repl.go
@@ -0,0 +1,30 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"interpreter/lexer"
+	"interpreter/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		input := scanner.Scan()
+		if !input {
+			return
+		}
+
+		line := scanner.Text()
+		lex := lexer.New(line)
+
+		for tok := lex.NextToken(); tok.Type != token.EOF; tok = lex.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
