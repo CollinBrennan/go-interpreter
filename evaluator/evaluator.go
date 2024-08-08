2e1f0e80b0060d26e85a2e74f977bfd540c2e2e7
Collin Brennan <collinbrennan8@gmail.com>
2024-08-08 11:00:36 -0400

    Evaluate integer and boolean literals

---
+++ evaluator/evaluator.go
@@ -0,0 +1,32 @@
+package evaluator
+
+import (
+	"interpreter/ast"
+	"interpreter/object"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return &object.Boolean{Value: node.Value}
+	default:
+		return nil
+	}
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+	return result
+}
---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,78 @@
+package evaluator
+
+import (
+	"interpreter/lexer"
+	"interpreter/object"
+	"interpreter/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+
+	if !ok {
+		t.Errorf("Expected Integer object. Got %T (%+v) instead.", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("Object has wrong value. Expected %d. Got %d instead.", result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+
+	if !ok {
+		t.Errorf("Expected Boolean object. Got %T (%+v) instead.", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("Object has wrong value. Expected %t. Got %t instead.", result.Value, expected)
+		return false
+	}
+
+	return true
+}
