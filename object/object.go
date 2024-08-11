2a14e3001cb13bf4772bf9f57af395f2a35970ef
Collin Brennan <collinbrennan8@gmail.com>
2024-08-11 09:51:59 -0400

    Evaluate function calls & environment passing

--- object/object.go
+++ object/object.go
@@ -1,6 +1,11 @@
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"interpreter/ast"
+	"strings"
+)
 
 type ObjectType string
 
@@ -15,6 +20,7 @@ const (
 	BOOLEAN_OBJ      = "BOOLEAN"
 	RETURN_VALUE_OBJ = "RETURN VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 // null
@@ -54,3 +60,29 @@ type Error struct {
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "Error: " + e.Message }
+
+// function
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var buffer bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	buffer.WriteString("fn")
+	buffer.WriteString("(")
+	buffer.WriteString(strings.Join(params, ", "))
+	buffer.WriteString(") {\n")
+	buffer.WriteString(f.Body.String())
+	buffer.WriteString("\n}")
+
+	return buffer.String()
+}
